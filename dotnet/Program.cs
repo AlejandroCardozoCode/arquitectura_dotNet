30b217174b4859c5e6b75af1d2f70af43431fbac
Alejandro <57674935+McFlyWare@users.noreply.github.com>
2022-05-12 22:58:42 -0500

    2

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -10,10 +10,18 @@ namespace dotnet
         {
             var db = new arquitecturaContext();
             var usuarios = db.Usuarios.ToList();
+            Console.WriteLine("Usuarios del sistema");
             foreach (var us in usuarios)
             {
                 System.Console.WriteLine("Id: " + us.Idusuario + " Nombre: " + us.Nombre);
             }
+            var productos = db.Productos.ToList();
+            Console.WriteLine("Productos del sistema");
+            foreach (var pro in productos)
+            {
+                System.Console.WriteLine("Id: " + pro.Idproducto + " Nombre: " + pro.Nombre);
+            }
+
         }
     }
 }
